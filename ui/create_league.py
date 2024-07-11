8ec7cec75f3599dd52f9fb998d660852d2e7bd51
Ancalagon02 <Ancalagon102@gmail.com>
2024-07-11 22:52:44 +0200

    updated UI

--- ui/create_league.py
+++ ui/create_league.py
@@ -11,7 +11,7 @@ class CreateLeague(QWidget):
         self.create_country_button = QPushButton()
         self.competition_name_label = QLabel()
         self.competition_name_input = QLineEdit()
-        self.compeition_name_Button = QPushButton()
+        self.competition_name_Button = QPushButton()
         self.select_team_label = QLabel()
         self.select_team_dropdow = QComboBox()
         self.select_team_button = QPushButton()
@@ -31,40 +31,22 @@ class CreateLeague(QWidget):
 
     def set_layout(self):
         self.master = QHBoxLayout()
-
+        #change to grid
         self.col1 = QVBoxLayout()
-        self.col1_row1 = QHBoxLayout()
-
-        self.col1_row1_col1 = QVBoxLayout()
-        self.col1_row1_col1.addWidget(self.select_country_label)
-        self.col1_row1_col1.addWidget(self.select_country_dropdown)
-        self.col1_row1_col1.addWidget(self.create_country_label)
-        self.col1_row1_col1.addWidget(self.create_country_input)
-        self.col1_row1_col1.addWidget(self.create_country_button)
-        self.col1_row1_col1.addWidget(self.select_competition_name_label)
-        self.col1_row1.addLayout(self.col1_row1_col1)
-
-        self.col1_row1_col2 = QVBoxLayout()
-        self.col1_row1_col2.addWidget(self.competition_name_label)
-        self.col1_row1_col2.addWidget(self.competition_name_input)
-        self.col1_row1_col2.addWidget(self.compeition_name_Button)
-        self.col1_row1_col2.addWidget(self.select_team_label)
-        self.col1_row1_col2.addWidget(self.select_team_dropdow)
-        self.col1_row1_col2.addWidget(self.select_team_button)
-        self.col1_row1_col2.addWidget(self.select_competition_name_text_label)
-        self.col1_row1.addLayout(self.col1_row1_col2)
-        
-        self.col1.addLayout(self.col1_row1)
-        self.col1.addWidget(self.create_competition_button)
-
+        self.col1.addWidget(self.select_country())
+        self.col1.addWidget(self.create_country())
+        self.col1.addWidget(self.select_competition_name_label)
 
         self.col2 = QVBoxLayout()
-        self.col2.addWidget(self.teams_label)
-        self.col2.addWidget(self.teams_listview)
+        self.col2.addWidget(self.competition_name())
+        self.col2.addWidget(self.select_team())
+        self.col2.addWidget(self.select_competition_name_text_label)
 
         self.master.addLayout(self.col1)
         self.master.addLayout(self.col2)
 
+        self.master.addWidget(self.teams_listview)
+        
         self.setLayout(self.master)
 
 
@@ -73,9 +55,56 @@ class CreateLeague(QWidget):
         self.create_country_label.setText("Maak Land")
         self.create_country_button.setText("Maak")
         self.competition_name_label.setText("Competitie Naam")
-        self.compeition_name_Button.setText("Maak")
+        self.competition_name_Button.setText("Maak")
         self.select_team_label.setText("Selecteer Ploeg")
         self.select_team_button.setText("Voeg Toe")
         self.select_competition_name_label.setText("Competitie Naam")
         self.create_competition_button.setText("Maak Competitie")
         self.teams_label.setText("Teams")
+
+
+    def select_country(self) -> QWidget:
+        widget = QWidget()
+        master = QVBoxLayout()
+        master.addWidget(self.select_country_label)
+        master.addWidget(self.select_country_dropdown)
+
+        widget.setLayout(master)
+
+        return widget
+
+    
+    def create_country(self) -> QWidget:
+        widget = QWidget()
+        master = QVBoxLayout()
+        master.addWidget(self.create_country_label)
+        master.addWidget(self.create_country_input)
+        master.addWidget(self.create_country_button)
+
+        widget.setLayout(master)
+
+        return widget
+
+
+    def competition_name(self) -> QWidget:
+        widget = QWidget()
+        master = QVBoxLayout()
+        master.addWidget(self.competition_name_label)
+        master.addWidget(self.competition_name_input)
+        master.addWidget(self.competition_name_Button)
+
+        widget.setLayout(master)
+
+        return widget
+
+
+    def select_team(self) -> QWidget:
+        widget = QWidget()
+        master = QVBoxLayout()
+        master.addWidget(self.select_team_label)
+        master.addWidget(self.select_team_dropdow)
+        master.addWidget(self.select_team_button)
+
+        widget.setLayout(master)
+
+        return widget
