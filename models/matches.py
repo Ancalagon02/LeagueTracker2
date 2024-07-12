f8ef9f5da30a267a4f73efa6e64c19a6465822f7
Ancalagon02 <Ancalagon102@gmail.com>
2024-07-12 23:53:07 +0200

    updated matches

--- models/matches.py
+++ models/matches.py
@@ -1,9 +1,11 @@
 from dataclasses import dataclass, field
 from datetime import date
 from models.club import Club
+from models.league import League
 
 @dataclass
 class Matches:
     id: int = 0
+    league: League = field(default_factory= League)
     clubs: list[Club] = field(default_factory=lambda: [])
     data: date = date.today()
