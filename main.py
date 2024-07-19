6cc8250e3f3aa96a7ef83cdbfd27ed19b205c5ee
Ancalagon02 <Ancalagon102@gmail.com>
2024-07-20 00:19:14 +0200

    added create team

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from PyQt6.QtWidgets import QApplication
 from ui.main_window import MainWindow
 from ui.create_country import CreateCountry
+from ui.create_team import CreateTeam
 import sys
 
 def main():
     app = QApplication([])
-    window = CreateCountry()
+    window = CreateTeam()
     window.show()
 
     sys.exit(app.exec())
