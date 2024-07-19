e2fe202e898c5248c7ffefe944a5b996205804e4
Ancalagon02 <Ancalagon102@gmail.com>
2024-07-19 18:52:40 +0200

    update rep

--- models/country.py
+++
@@ -1,6 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class Country():
-    id: int = 0
-    name: str = ""
--- models/league.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass, field
-from .country import Country
-
-@dataclass
-class League:
-    id: int = 0 
-    country: Country = field(default_factory= Country)
-    name: str = ""
